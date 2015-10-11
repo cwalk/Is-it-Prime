54761c66de63a3052bd69ae68ea49967fede3355
Clayton Walker <claytonwalkerucf@knights.ucf.edu>
2015-10-11 01:08:37 -0400

    Finished app

--- Is it Prime/Is it Prime/ViewController.swift
+++ Is it Prime/Is it Prime/ViewController.swift
@@ -19,7 +19,39 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
 
-
+    @IBAction func isItPrime(sender: UIButton) {
+        
+        if let number = Int(numberTextField.text!) {
+        
+            var isPrime = true
+            
+            if number == 1 {
+                isPrime = false
+            }
+        
+            if (number != 2 && number != 1) {
+            
+                for var i = 2; i<number;i++ {
+                    if (number%i==0){
+                        isPrime = false
+                    }
+                }
+            }
+        
+            if isPrime {
+                resultLabel.text = "\(number) is Prime!"
+            }
+            else {
+                resultLabel.text = "\(number) is not Prime"
+            }
+        }
+        else {
+            resultLabel.text = "Please enter a whole number"
+        }
+    }
 }
 
